cb7a8402f8fa2b138c2179584c04f3217f270797
Sathursanak <132430609+Sathursanak@users.noreply.github.com>
2025-01-12 21:00:30 +0530

    ready to deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+  images:{
+    loader: 'custom',
+    loaderFile: './loader.js',
+    remotePatterns: [
+      {
+       
+        protocol: "https",
+        hostname: "images.pexels.com"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
